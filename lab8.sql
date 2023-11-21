b7c7ef5eda85f023fbb2b9158202377558221ea0
SonyG14 <141037332+SonyG14@users.noreply.github.com>
2023-11-21 08:40:39 +0200

    Update lab8.sql

--- lab8.sql
+++ lab8.sql
@@ -1,25 +1,21 @@
 CREATE TABLE Users (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL
- 
 )
-
 
 CREATE TABLE Projects (
     project_id INT PRIMARY KEY AUTO_INCREMENT,
-    project_name VARCHAR(255) NOT NULL
-   
+    name VARCHAR(255) NOT NULL
 )
-
 CREATE TABLE Tasks (
     task_id INT PRIMARY KEY AUTO_INCREMENT,
-    task_name VARCHAR(255) NOT NULL,
-    author_id INT,
+    name VARCHAR(255) NOT NULL,
+    user_id INT,
     project_id INT,
     deadline DATE,
     status VARCHAR(50),
     priority INT,
-    FOREIGN KEY (author_id) REFERENCES Users(user_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (project_id) REFERENCES Projects(project_id)
 );
 
@@ -27,7 +23,7 @@ CREATE TABLE Task_Comments (
     comment_id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
-    comment_text TEXT,
+    comment TEXT,
     comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
     FOREIGN KEY (user_id) REFERENCES Users(user_id)
@@ -44,8 +40,8 @@ CREATE TABLE Task_Users (
 
 CREATE TABLE Files (
     file_id INT PRIMARY KEY AUTO_INCREMENT,
-    file_name VARCHAR(255) NOT NULL,
-    file_url VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
     project_id INT,
     task_id INT,
     FOREIGN KEY (project_id) REFERENCES Projects(project_id),
