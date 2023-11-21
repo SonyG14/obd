f44c84d3a38609c3514158da7f0c851cd0bd5af6
SonyG14 <141037332+SonyG14@users.noreply.github.com>
2023-11-21 09:31:09 +0200

    Update lab8.sql

--- lab8.sql
+++ lab8.sql
@@ -1,49 +1,48 @@
-CREATE TABLE Users (
-    user_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL
-)
+);
 
-CREATE TABLE Projects (
+CREATE TABLE projects (
     project_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
-)
-CREATE TABLE Tasks (
+);
+CREATE TABLE tasks (
     task_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     user_id INT,
     project_id INT,
-    deadline DATE,
+    deadline DATETIME,
     status VARCHAR(50),
     priority INT,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id)
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (project_id) REFERENCES projects(project_id)
 );
 
-CREATE TABLE Task_Comments (
+CREATE TABLE task_comments (
     comment_id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
     comment TEXT,
     comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-
-CREATE TABLE Task_Users (
+CREATE TABLE task_users (
     task_user_id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
-    FOREIGN KEY (task_id) REFERENCES Tasks(task_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
-)
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
 
-CREATE TABLE Files (
+CREATE TABLE files (
     file_id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL,
     project_id INT,
     task_id INT,
-    FOREIGN KEY (project_id) REFERENCES Projects(project_id),
-    FOREIGN KEY (task_id) REFERENCES Tasks(task_id)
+    FOREIGN KEY (project_id) REFERENCES projects(project_id),
+    FOREIGN KEY (task_id) REFERENCES tasks(task_id)
 );
