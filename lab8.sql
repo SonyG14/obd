2dc5fb8374779648e4539d01a8109ea38bb25c1f
SonyG14 <141037332+SonyG14@users.noreply.github.com>
2023-11-27 00:55:43 +0200

    Update lab8.sql

--- lab8.sql
+++ lab8.sql
@@ -1,55 +1,55 @@
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    username VARCHAR(255) NOT NULL
+    author VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE projects (
-    project_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
 CREATE TABLE tasks (
-    task_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    author_id INT,
-    project_id INT,
+    author INT,
+    project INT,
     deadline DATETIME,
     status VARCHAR(50),
-    priority INT,
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (project_id) REFERENCES projects(project_id)
+    FOREIGN KEY (author) REFERENCES users(id),
+    FOREIGN KEY (project) REFERENCES projects(id)
 );
 
-CREATE TABLE task_comments (
-    comment_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE comments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
     comment TEXT,
-    comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    date DATETIME,
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE task_users (
-    task_user_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
-    FOREIGN KEY (task_id) REFERENCES tasks(task_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
 CREATE TABLE project_users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     project_id INT,
     user_id INT,
-    FOREIGN KEY (project_id) REFERENCES projects(project_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE files (
-    file_id INT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     url VARCHAR(255) NOT NULL,
     project_id INT,
     task_id INT,
-    FOREIGN KEY (project_id) REFERENCES projects(project_id),
-    FOREIGN KEY (task_id) REFERENCES tasks(task_id)
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
