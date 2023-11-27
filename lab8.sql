baa475f61396b8d9388ea95151a443a959f95474
SonyG14 <141037332+SonyG14@users.noreply.github.com>
2023-11-28 00:53:45 +0200

    Update lab8.sql

--- lab8.sql
+++ lab8.sql
@@ -7,22 +7,25 @@ CREATE TABLE projects (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE tasks (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    author INT,
-    project INT,
+    author INT NOT NULL,
+    project INT NOT NULL,
+    user_id INT NOT NULL, 
     deadline DATETIME,
     status VARCHAR(50),
     FOREIGN KEY (author) REFERENCES users(id),
-    FOREIGN KEY (project) REFERENCES projects(id)
+    FOREIGN KEY (project) REFERENCES projects(id),
+    FOREIGN KEY (user_id) REFERENCES users(id) 
 );
 
 CREATE TABLE comments (
     id INT PRIMARY KEY AUTO_INCREMENT,
     task_id INT,
     user_id INT,
-    comment TEXT,
+    comment TEXT NOT NULL,
     date DATETIME,
     FOREIGN KEY (task_id) REFERENCES tasks(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
@@ -50,6 +53,8 @@ CREATE TABLE files (
     url VARCHAR(255) NOT NULL,
     project_id INT,
     task_id INT,
+    user_id INT,
     FOREIGN KEY (project_id) REFERENCES projects(id),
     FOREIGN KEY (task_id) REFERENCES tasks(id)
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
